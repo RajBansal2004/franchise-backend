5475e1ce23e4777afad8fe20654f51cc0588cb3b
RajBansal2004 <138199298+RajBansal2004@users.noreply.github.com>
2026-01-23 11:11:58 +0530

    latest changes

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -4,6 +4,5 @@ const auth = require('../middlewares/auth.middleware');
 const permit = require('../middlewares/role.middleware');
 
 router.get('/profile', auth, permit('USER'), ctrl.getUserDashboard);
-router.get('/royalty/summary',  ctrl.getRoyaltySummary);
-
+router.get('/royalty/summary', auth, permit('USER'), ctrl.getRoyaltySummary);
 module.exports = router;
