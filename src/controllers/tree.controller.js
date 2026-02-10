f5016f5ec9553e5924f17010f893355f6f6aebf1
RajBansal2004 <138199298+RajBansal2004@users.noreply.github.com>
2026-02-10 14:19:08 +0530

    latest changes

--- src/controllers/tree.controller.js
+++ src/controllers/tree.controller.js
@@ -3,7 +3,7 @@ const User = require('../models/User');
 
 exports.getMyTree = async (req, res) => {
 const user = await User.findById(req.user.id)
-.populate('leftChild')
-.populate('rightChild');
+.populate('leftChildren')
+.populate('rightChildren');
 res.json(user);
 };
