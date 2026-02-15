010f36e55bfda4f84b8614ad44e4a2ebd8be1a0f
RajBansal2004 <138199298+RajBansal2004@users.noreply.github.com>
2026-02-15 16:04:14 +0530

    latest commit

--- src/routes/franchiseRoutes.js
+++ src/routes/franchiseRoutes.js
@@ -2,6 +2,8 @@ const express = require('express');
 const router = express.Router();
 const { getFranchiseDashboard } = require('../controllers/franchiseDashboard');
 const auth = require('../middlewares/auth.middleware');
+const { getProfile } = require('../controllers/user.controller');
+
 
 router.get('/dashboard', auth, getFranchiseDashboard);
 router.get('/profile', auth, getProfile);
