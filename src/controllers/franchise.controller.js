5b0d3cc83bb6e987d3cface0c2ddcad81893ea7d
RajBansal2004 <138199298+RajBansal2004@users.noreply.github.com>
2026-02-19 10:50:55 +0530

    latest commit

--- src/controllers/franchise.controller.js
+++ src/controllers/franchise.controller.js
@@ -147,4 +147,29 @@ exports.getMyStock = async (req, res) => {
 };
 
 
+exports.getFranchiseDashboard = async (req, res) => {
+  try {
+    const franchiseId = req.user.id;
 
+    const totalOrders = await Order.countDocuments({
+      franchiseId,
+    });
+
+   const activeIds = await User.countDocuments({
+  isActive: true,
+  activatedBy: new mongoose.Types.ObjectId(franchiseId),
+});
+
+
+    const franchise = await User.findById(franchiseId);
+
+    res.json({
+      totalOrders,
+      activeIds,
+      stockItems: franchise.stock,
+      monthlyIncome: 0,
+    });
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+};
--- src/routes/franchiseRoutes.js
+++ src/routes/franchiseRoutes.js
@@ -1,13 +1,12 @@
 const express = require('express');
 const router = express.Router();
-const { getFranchiseDashboard } = require('../controllers/franchiseDashboard');
 const auth = require('../middlewares/auth.middleware');
 const { getProfile } = require('../controllers/user.controller');
 const ctrl = require('../controllers/franchise.controller');
 const permit = require('../middlewares/role.middleware');
 
 router.get('/stock', auth, ctrl.getFranchiseStock);
-router.get('/dashboard', auth, getFranchiseDashboard);
+router.get('/dashboard', auth, ctrl.getFranchiseDashboard);
 router.get('/profile', auth, getProfile);
 router.get('/search-user/:uniqueId', auth, ctrl.searchUserByUniqueId);
 router.post('/activate-id', auth, permit('FRANCHISE'), ctrl.activateUserId);
