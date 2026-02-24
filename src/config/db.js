afb591b1e4a02670a2a995ab7c08b77f3a34b9d9
RajBansal2004 <138199298+RajBansal2004@users.noreply.github.com>
2026-02-24 15:13:07 +0530

    new database latest

--- src/config/db.js
+++ src/config/db.js
@@ -1,39 +1,19 @@
-// const mongoose = require('mongoose');
-
-// const connectDB = async (uri) => {
-//   try {
-//     mongoose.set('strictQuery', false);
-//     await mongoose.connect(uri, {
-//       useNewUrlParser: true,
-//       useUnifiedTopology: true,
-//     });
-//     console.log('MongoDB connected');
-//      console.log("📌 Database Name:", conn.connection.name);
-//   console.log("🌍 Host:", conn.connection.host);
-//   } catch (err) {
-//     console.error('MongoDB connection error:', err);
-//     process.exit(1);
-//   }
-// };
-
-// module.exports = connectDB;
-
 const mongoose = require('mongoose');
 
 const connectDB = async (uri) => {
   try {
     mongoose.set('strictQuery', false);
-
-    const conn = await mongoose.connect(uri);
-
-    console.log('✅ MongoDB Connected');
-    console.log('📌 Database Name:', conn.connection.name);
-    console.log('🌍 Host:', conn.connection.host);
-
+    await mongoose.connect(uri, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log('MongoDB connected');
+    console.log("MONGO_URI:", process.env.MONGO_URI);
   } catch (err) {
-    console.error('❌ MongoDB connection error:', err.message);
+    console.error('MongoDB connection error:', err);
     process.exit(1);
   }
 };
 
 module.exports = connectDB;
+
