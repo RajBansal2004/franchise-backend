60156abc156f88da39e45d94fd25f4c6a1cdc2ac
RajBansal2004 <138199298+RajBansal2004@users.noreply.github.com>
2026-03-16 13:34:56 +0530

    latest upadate

--- src/routes/order.routes.js
+++ src/routes/order.routes.js
@@ -1,10 +1,9 @@
 const express = require('express');
 const router = express.Router();
-
 const orderCtrl = require('../controllers/order.controller');
 const auth = require('../middlewares/auth.middleware');
 const permit = require('../middlewares/role.middleware');
-const upload = require("../middlewares/upload");
+const upload = require("../middlewares/uploadCloudinary");
 
 
 // ⭐ CREATE ORDER
