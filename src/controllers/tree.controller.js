21c2a48d15b131a8620c6d561a85bdc0a35f9e97
RajBansal2004 <138199298+RajBansal2004@users.noreply.github.com>
2026-04-08 13:17:21 +0530

    updated code

--- src/controllers/tree.controller.js
+++ src/controllers/tree.controller.js
@@ -1,9 +1,25 @@
 const User = require('../models/User');
 
 
+// exports.getMyTree = async (req, res) => {
+// const user = await User.findById(req.user.id)
+// .populate('leftChildren')
+// .populate('rightChildren');
+// res.json(user);
+// };
+
+
 exports.getMyTree = async (req, res) => {
-const user = await User.findById(req.user.id)
-.populate('leftChildren')
-.populate('rightChildren');
-res.json(user);
+  const user = await User.findById(req.user.id)
+    .populate('leftChildren')
+    .populate('rightChildren');
+
+  // 👉 FIRST CHILD nikal do
+  const formattedUser = {
+    ...user._doc,
+    leftChild: user.leftChildren[0] || null,
+    rightChild: user.rightChildren[0] || null,
+  };
+
+  res.json(formattedUser);
 };
