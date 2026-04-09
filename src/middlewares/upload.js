81dc9c1c1564750dbbef04eb5be6c30f0e68bac6
RajBansal2004 <138199298+RajBansal2004@users.noreply.github.com>
2026-04-09 11:49:01 +0530

    updated code

--- src/middlewares/upload.js
+++ src/middlewares/upload.js
@@ -15,7 +15,8 @@ const fileFilter = (req, file, cb) => {
   if (
     file.mimetype === 'image/jpeg' ||
     file.mimetype === 'image/png' ||
-    file.mimetype === 'image/jpg'
+    file.mimetype === 'image/jpg' ||
+    file.mimetype === 'application/pdf'
   ) {
     cb(null, true);
   } else {
